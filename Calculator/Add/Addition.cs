c5be98ec3bd80e5a779b9da0e6328e642a1e34cf
Binett <73723725+Binett@users.noreply.github.com>
2021-12-06 17:22:37 +0100

    Added testproject

---
+++ Calculator/Add/Addition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Add
+{
+    public class Addition
+    {
+        private readonly int _firstnumber;
+        private readonly int _secondnumber;
+
+        public Addition()
+        {
+
+        }
+
+        public Addition(int firstnumber, int secondnumber)
+        {
+            _firstnumber = firstnumber;
+            _secondnumber = secondnumber;
+        }
+
+        public int Calculate()
+        {
+            if (_firstnumber <= 0 || _secondnumber <= 0) return 0;
+            return _firstnumber + _secondnumber; 
+        }
+
+    }
+}
